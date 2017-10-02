dd201c56354bb5b044ada777111213fccc4eba16
COREY COSMAN <cosman.corey@gmail.com>
2017-10-02 18:09:37 -0400

    add ruby

---
+++ thursday_sept_21_2017_trains.rb
@@ -0,0 +1,21 @@
+
+array = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+# train_111_direction = array.last [:direction]
+new_array = array.select do |v|
+  v == "north"
+  end
+end
+
+
+# p train_111_direction
+p new_array
