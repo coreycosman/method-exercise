806b7561eec61a8334ab99c10df21c21abcfee50
COREY COSMAN <cosman.corey@gmail.com>
2017-10-03 23:37:19 -0400

    add ruby for tuesday_sept_26_2017

--- tuesday_sept_26_2017.rb
+++ tuesday_sept_26_2017.rb
@@ -1,30 +1,45 @@
 class Person
 
-  def self.create
-    person = Person.new(name, emotions)
+  def self.create(name)
+    person = Person.new(name)
   end
 
   def initialize(name)
     @name = name
     @emotions = {}
 
-    @emotions[:happy] = 3
-    @emotions[:mad] = 1
-    @emotions[:sad] = 1
-  end  
+    @emotions[:happy] = rand(1..3)
+    @emotions[:mad] = rand(1..3)
+    @emotions[:sad] = rand(1..3)
+  end
+
+  def emotions
+    @emotions
+  end
+
+
+  def feeling
+    @emotions.each do |emotion, value|
+    #  puts "I am feeling #{word(value)} #{emotion}"
+    case [emotion, value]
+    when [:happy, 1] then puts "I am feeling low happiness"
+    when [:happy, 2] then puts "I am feeling medium happiness"
+    when [:happy, 3] then puts "I am feeling high happiness"
+    when [:mad, 1] then puts "I am feeling low madness"
+    when [:mad, 2] then puts "I am feeling medium madness"
+    when [:mad, 3] then puts "I am feeling high madness"
+    when [:sad, 1] then puts "I am feeling low sadness"
+    when [:sad, 2] then puts "I am feeling medium sadness"
+    when [:sad, 3] then puts "I am feeling high sadness"
+    end
+    end
+  end
 
 end
 
-def emotions
-  @emotions
-end
-
-def output
-  @emotions.each do []
 
 
 
-
-
-
-p person_1 = Person.create()
+p person_1 = Person.create("name_1")
+p person_1.emotions
+p person_1.feeling
